13fa3146e974fe0ba4c8a0f7a9e0f06caeddc252
Vichhagar Nhin <46106879+Vichhagar@users.noreply.github.com>
2021-05-22 22:58:48 +0700

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ vid.volume = 0.2;
 
 // Set the date we're counting down to
 var countDownDate = new Date("May 23, 2021 00:00:00").getTime();
+console.log("countDownDate");
 
 // Update the count down every 1 second
 var x = setInterval(function() {
