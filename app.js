85f5d2fa8987a19a7b7936ee0903f8cea8599fbd
Vichhagar Nhin <46106879+Vichhagar@users.noreply.github.com>
2024-10-23 21:50:21 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ vid.volume = 0.2;
 // CountDown
 
 // Set the date we're counting down to
-var countDownDate = new Date("May 23, 2022 00:00:00").getTime();
+var countDownDate = new Date("May 23, 2025 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
